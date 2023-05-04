615cde1c87f89c193401bf9d77227fd953fc4d44
Kate Sullivan <sullivan.kate.e@gmail.com>
2023-05-04 16:15:59 -0700

    Start on populating dropdown menu with languages

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,48 @@
+// Get a reference to the HTML dropdown element where we will display the values
+const dropdown = document.getElementById("language-dropdown");
+
+// Get the collection reference for the desired collection
+const collectionRef = db.collection("languages");
 
 
-const express = require('express');
-const app = express();
-const port = 3000;
+function populateLanguageDropdown() {
+  // Fetch the documents from the collection
+  collectionRef.get().then((querySnapshot) => {
+    // Loop through each document and get its string fields
+    const values = [];
+    querySnapshot.forEach((doc) => {
+      const data = doc.data();
+      for (const key in data) {
+        if (typeof data[key] === "string") {
+          values.push(data[key]);
+        }
+      }
+    });
 
+    values.sort();
+
+
+    // Populate the dropdown with the values
+    values.forEach((value) => {
+      const option = document.createElement("option");
+      option.text = value;
+      dropdown.add(option);
+    });
+    option = document.createElement("option");
+    option.text = "";
+    option.selected = true;
+    dropdown.add(option);
+  });
+}
+
+// Call the populateDropdown function to start populating the dropdown
+populateLanguageDropdown();
+
+function saveItemDocumentIDAndRedirect() {
+  let params = new URL(window.location.href) //get the url from the search bar
+  let ID = $('#my-dropdown').val(); //get the value of the dropdown
+  console.log("dropdown:", ID)
+  localStorage.setItem('itemDocID', ID);
+  window.location.href = 'profilelistwithresource.html';
+  console.log(window.location.href)
+}
