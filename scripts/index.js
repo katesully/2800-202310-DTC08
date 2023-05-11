046500a5443f5c148dca03c2383b311ea4fc43a0
Kate Sullivan <sullivan.kate.e@gmail.com>
2023-05-11 15:18:23 -0700

    Add js to hide/show graphic and info when button clicked

--- scripts/index.js
+++ scripts/index.js
@@ -4,11 +4,13 @@
 
 function saveLanguageAndShowSignup() {
   console.log("saveLanguageAndShowSignup called");
-  // Show login and signup buttons
+  // Show login and signup buttons and info div
   const loginButton = document.getElementById("login-button");
   const signupButton = document.getElementById("signup-button");
+  const backgroundImage = document.getElementById("backgroundCard");
   loginButton.style.display = "inline-block";
   signupButton.style.display = "inline-block";
+  backgroundImage.style.display = "block";
 }
 
 // // Get a reference to the HTML dropdown element where we will display the values
