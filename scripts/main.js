4d6d794c47bc62c1b1b9b87b13e65dca79c761a4
cz1501 <122151441+cz1501@users.noreply.github.com>
2023-05-10 18:12:22 -0700

    Remove API key

--- scripts/main.js
+++ scripts/main.js
@@ -3,34 +3,33 @@
 // const dotenv = require('dotenv');
 // require('dotenv').config()
 // const API_KEY = process.env.OPENAI_API_KEY;
-const API_KEY = "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"
 // const submitBtn = document.getElementById('submitBtn');
 
 
 
-async function getMessage() {
-    console.log('clicked');
-    const options = {
-        method: 'POST',
-        headers: {
-            'Authorization': `Bearer ${API_KEY}`,
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            model: "gpt-3.5-turbo",
-            messages: [{ role: "user", content: "Hello!" }],
-            max_tokens: 100,
-        })
-    }
-    try {
-        const response = await fetch('https://api.openai.com/v1/chat/completions', options)
-        const data = await response.json();
-        console.log(data);
-    }
-    catch (error) {
-        console.log(error);
-    }
-}
+// async function getMessage() {
+//     console.log('clicked');
+//     const options = {
+//         method: 'POST',
+//         headers: {
+//             'Authorization': `Bearer ${API_KEY}`,
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({
+//             model: "gpt-3.5-turbo",
+//             messages: [{ role: "user", content: "Hello!" }],
+//             max_tokens: 100,
+//         })
+//     }
+//     try {
+//         const response = await fetch('https://api.openai.com/v1/chat/completions', options)
+//         const data = await response.json();
+//         console.log(data);
+//     }
+//     catch (error) {
+//         console.log(error);
+//     }
+// }
 
 // submitBtn.addEventListener('click', getMessage);
 
