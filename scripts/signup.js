b6a0e77a1ff10ed2529ce169403b1dd2e93da48f
VS-LIU <vctl95@gmail.com>
2023-05-10 20:08:13 -0700

    Import code from firebase

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,5 +1,20 @@
 // this code was written by Oceaan with help from
 //https://stackoverflow.com/questions/21727317/how-to-check-confirm-password-field-in-form-without-reloading-page
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const auth = getAuth();
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+
 
 var check = function () {
     if (document.getElementById('passwordInput').value ==
