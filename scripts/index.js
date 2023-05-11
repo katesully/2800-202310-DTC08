d835a4ded0f4bb73f8274cf6cf3131f3d38ccb0e
Kate Sullivan <sullivan.kate.e@gmail.com>
2023-05-11 13:17:44 -0700

    Update settings page and link to button

--- scripts/index.js
+++ scripts/index.js
@@ -1,47 +1,3 @@
-setTimeout(function() {
-  document.cookie = "googtrans=/; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-}, 1000);
-
-
-
-// const app = new Vue({
-//   el: '#app',
-//   data: {
-//     translatedContent: ''
-//   },
-//   mounted() {
-//     // set the default language to the user's preferred language
-//     const userLanguage = navigator.language.slice(0, 2);
-//     this.translateContent(userLanguage);
-//   },
-//   methods: {
-//     translateContent(targetLanguage) {
-//       // call your translation API here and update the translatedContent data property
-//       // using the content data property and targetLanguage parameter
-//       // for example, using the Deepl Translate API:
-//       const apiKey = 'DEEPL_AUTH_KEY';
-//       const sourceLanguage = 'en'; // assume the source language is English
-//       const contentToTranslate = this.content[sourceLanguage];
-//       axios.post(`https://api.deepl.com/v2/translate?auth_key=${apikey}`, {
-//         q: contentToTranslate,
-//         target: targetLanguage
-//       }).then(response => {
-//         this.translatedContent = response.data.data.translations[0].translatedText;
-//       }).catch(error => {
-//         console.error(error);
-//       });
-//     }
-//   }
-// });
-
-// Force hiding of "original text" popup for menus, etc. (very annoying)
-jQuery(selector).bind(
-  "mouseenter mouseleave",
-  function (event) {
-    if (event.type === 'mouseenter') { google_trans_tt.css('z-index', -1000); }
-    else { google_trans_tt.css('z-index', 1000); }
-  }
-);
 
 
 
@@ -55,44 +11,48 @@ function saveLanguageAndShowSignup() {
   signupButton.style.display = "inline-block";
 }
 
-// Get a reference to the HTML dropdown element where we will display the values
-const dropdown = document.getElementById("language-dropdown");
+// // Get a reference to the HTML dropdown element where we will display the values
+// const dropdown = document.getElementById("language-dropdown");
 
-// Get the collection reference for the desired collection
-const collectionRef = db.collection("Languages");
-
-
-function populateLanguageDropdown() {
-  // Fetch the documents from the collection
-  collectionRef.get().then((querySnapshot) => {
-    // Loop through each document and get its string fields
-    const values = [];
-    querySnapshot.forEach((doc) => {
-      const data = doc.data();
-      for (const key in data) {
-        if (typeof data[key] === "string") {
-          values.push(data[key]);
-        }
-      }
-    });
-
-    values.sort();
+// // Get the collection reference for the desired collection
+// // const collectionRef = db.collection("Languages");
 
 
-    // Populate the dropdown with the values
-    values.forEach((value) => {
-      const option = document.createElement("option");
-      option.text = value;
-      dropdown.add(option);
-    });
-    option = document.createElement("option");
-    option.text = "";
-    option.selected = true;
-    dropdown.add(option);
-  });
-}
+// function populateLanguageDropdown() {
+//   // Fetch the documents from the collection
+//   collectionRef.get().then((querySnapshot) => {
+//     // Loop through each document and get its string fields
+//     const values = [];
+//     querySnapshot.forEach((doc) => {
+//       const data = doc.data();
+//       for (const key in data) {
+//         if (typeof data[key] === "string") {
+//           values.push(data[key]);
+//         }
+//       }
+//     });
 
-// Call the populateDropdown function to start populating the dropdown
-populateLanguageDropdown();
+//     values.sort();
 
+
+//     // Populate the dropdown with the values
+//     values.forEach((value) => {
+//       const option = document.createElement("option");
+//       option.text = value;
+//       dropdown.add(option);
+//     });
+//     option = document.createElement("option");
+//     option.text = "";
+//     option.selected = true;
+//     dropdown.add(option);
+//   });
+// }
+
+// // Call the populateDropdown function to start populating the dropdown
+// populateLanguageDropdown();
+
+
+//create bootstrap popup that contains Google Translate API
+
+  
 
