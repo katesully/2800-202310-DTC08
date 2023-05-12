a1112dc35ae9351d84b6485d33c9f16d6c91418c
cz1501 <122151441+cz1501@users.noreply.github.com>
2023-05-12 04:37:51 -0700

    Connect with saved roadmaps page with footer

--- app.js
+++ app.js
@@ -61,11 +61,9 @@ async function getMessage(message) {
         })
     }
     try {
-        console.log('step 1')
+
         const response = await fetch('https://api.openai.com/v1/chat/completions', options)
-        console.log('step 2')
         const data = await response.json();
-        console.log('step 3')
         console.log(data);
         return data;
     }
@@ -81,16 +79,13 @@ function createRoadmapObject(message) {
         description: "",
         steps: []
     };
-    console.log('step 5')
+
     var messageArray = message.split("\n").filter(line => line.length > 0);
-    console.log('step 6')
     roadmapObject.title = messageArray[0].split(": ")[1];
-    console.log('step 7')
     roadmapObject.description = messageArray[1].split(": ")[1];
-    console.log('step 8')
+
 
     for (var i = 2; i < messageArray.length; i++) {
-        console.log(`step 8.${i}`)
         roadmapObject.steps.push(messageArray[i].split(". ")[1]);
     }
 
@@ -102,9 +97,8 @@ app.post('/sendRequest', async (req, res) => {
     var userInput = req.body.hiddenField || req.body.textInput;
 
     let returnMessage = await getMessage(userInput);
-    console.log('step 4')
     let roadmapObject = createRoadmapObject(returnMessage.choices[0].message.content);
-    console.log('step 9')
+
     console.log(roadmapObject)
     res.render('./main.ejs', {
         steps: roadmapObject.steps.slice(0, roadmapObject.steps.length),
