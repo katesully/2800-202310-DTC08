67c253e6acc7b69c61d9350f86f34a98ca0b6e82
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2023-05-12 09:10:54 -0700

    Create file for dotenv Firebase keys.

---
+++ scripts/firebase_dotenv_keys.js
@@ -0,0 +1,16 @@
+const dotenv = require('dotenv')
+
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_DOMAIN,
+    projectId: process.env.FIREBASE_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKEY,
+    messagingSenderId: process.env.FIREBASE_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+const storage = firebase.storage();
