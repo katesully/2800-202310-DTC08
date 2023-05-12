0730ef03a6a438f311d85620fb49845eff426207
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2023-05-12 09:17:46 -0700

    Change old firebase file to use dotenv.

--- scripts/firebase_authentication.js
+++ scripts/firebase_authentication.js
@@ -12,5 +12,5 @@ const firebaseConfig = {
 const app = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 const auth = firebase.auth();
-
 const storage = firebase.storage();
+
