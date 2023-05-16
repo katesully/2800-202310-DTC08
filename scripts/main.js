e9251f0cd34372aabae8fcc7a853bd46fcacd038
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2023-05-15 17:15:44 -0700

    Add event listener - checked and unchecked boxes.

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,17 @@
+// add a blank event listener to all checkboxes
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener("click", function () {
+        var checkboxId = this.id;
+        if (this.checked) {
+            console.log("checkbox", this.id, "checked");
+        }
+        if (!this.checked) {
+            console.log("checkbox", this.id, "unchecked");
+        }
+    });
+});
+
 
 
