6d4d25b03613964dab997ba4c2028f0ac0491561
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2023-05-16 12:46:05 -0700

    Make ray only appear on sun click.

--- scripts/main.js
+++ scripts/main.js
@@ -19,6 +19,17 @@ sun.addEventListener("click", function () {
     console.log("sun clicked");
     // access circle element
     sun.classList.add("IncreaseSunSize");
+
+    //access element with id expanding-ray
+    const expandingRay = document.getElementById("expanding-ray");
+    //remove hidden class from expanding-ray
+    expandingRay.classList.remove("hidden");
+
+    //after 5 seconds, add hidden class to expanding-ray
+    setTimeout(function () {
+        expandingRay.classList.add("hidden");
+    }
+        , 5000);
 }
 );
 
