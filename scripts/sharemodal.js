a9ef528eb6441bc19e2abd6d2c1346adb8a5dde8
Kate Sullivan <sullivan.kate.e@gmail.com>
2023-05-17 12:22:06 -0700

    Remove bars and update roadmaps

--- scripts/sharemodal.js
+++ scripts/sharemodal.js
@@ -1,4 +1,4 @@
-$('#exampleModal').on('show.bs.modal', function (event) {
+$('#shareModal').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget) // Button that triggered the modal
     var recipient = button.data('whatever') // Extract info from data-* attributes
     // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
