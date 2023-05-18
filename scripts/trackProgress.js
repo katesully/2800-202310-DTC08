6009d8fac7dfaa69fd3c167544d6f198435630e6
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2023-05-18 16:47:11 -0700

    Write saveProgress function.

--- scripts/trackProgress.js
+++ scripts/trackProgress.js
@@ -1,3 +1,24 @@
-function saveProgress() {
+async function saveProgress(mapID) {
     console.log('Saving progress...');
+
+    //get all the checkboxes and save their state
+    var checkboxes = document.querySelectorAll('input[type=checkbox]');
+    var checkboxStates = [];
+    checkboxes.forEach((checkbox) => {
+        checkboxeStates.push(checkbox.checked);
+    }
+    );
+
+    //pass the checkboxStates array to the backend
+    await axios.post("/saveProgress", { mapID: mapID, checkboxStates: checkboxStates }, {
+        headers: {
+            'Content-Type': 'application/json',
+
+        }
+    }).then(response => {
+        console.log(response);
+    }).catch(error => {
+        console.log(error);
+    });
+
 }
