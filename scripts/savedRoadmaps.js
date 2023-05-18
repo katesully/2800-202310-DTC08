1ca53b19aa8aa3d14cce784014445a17965512d7
cz1501 <122151441+cz1501@users.noreply.github.com>
2023-05-18 12:57:58 -0700

    Code use post to delete bookmark client side

--- scripts/savedRoadmaps.js
+++ scripts/savedRoadmaps.js
@@ -1,15 +1,40 @@
-function shareRoadmap() {
+let maptodelete;
+let maptoshare;
+
+
+function shareRoadmap(shareButton) {
     console.log("share roadmap clicked")
     $('#shareModal').modal('toggle')
 
 }
 
-function deleteRoadmap() {
+function deleteRoadmap(deleteButton) {
     console.log("delete roadmap clicked")
     $('#deleteModal').modal('toggle')
+    maptodelete = deleteButton.closest('.card');
+    console.log(maptodelete.getAttribute("value"));
 
 }
 
-function confirmDelete() {
+async function confirmDelete() {
     console.log("confirm delete clicked")
-}
+    let mapid = maptodelete.getAttribute("value");
+    console.log(mapid);
+    await axios.post("/deleteBookmark", {mapid: mapid}, {
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
+        .then(response => {
+            console.log(response);
+            maptodelete.remove();
+        })
+        .catch(error => {
+
+            alert("HTTP-Error: " + error);
+
+        });
+};
+
+
+
