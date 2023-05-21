01e11444d78462ce2a961f7d5178d264803cd932
VS-LIU <vctl95@gmail.com>
2023-05-21 15:34:38 -0700

    Remove old comments and add documentation

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,6 @@ if (window.location.pathname === "/main") {
     document.getElementById("btnBack").disabled = true;
 }
 
-
 // Event Listener (click) for the back button to return to the previous page in the browser history
 document.getElementById("btnBack").addEventListener("click", function() {
    window.history.back();
