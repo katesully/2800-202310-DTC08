036c468a23c9b8cb193ebf874721bca3d12b7c9a
VS-LIU <vctl95@gmail.com>
2023-05-23 11:28:21 -0700

    Fix header for /main

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,10 @@
 // Check if the current page is the main page, if so, hide the back button
 if (window.location.pathname === "/main") {
-    document.getElementById("btnBack").style.display = "none";
-    document.getElementById("btnBack").disabled = true;
+  document.getElementById("btnBack").style.display = "none";
+  document.getElementById("btnBack").disabled = true;
 }
 
 // Event Listener (click) for the back button to return to the previous page in the browser history
-document.getElementById("btnBack").addEventListener("click", function() {
-   window.history.back();
- });
+document.getElementById("btnBack").addEventListener("click", function () {
+  window.history.back();
+});
