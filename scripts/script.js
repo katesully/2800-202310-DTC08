6cd6b00bf10c70484956b430cedea789a07f9db6
VS-LIU <vctl95@gmail.com>
2023-05-23 11:40:36 -0700

    Remove old code and refactor

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,3 @@
-// Check if the current page is the main page, if so, hide the back button
-if (window.location.pathname === "/main") {
-  document.getElementById("btnBack").style.display = "none";
-  document.getElementById("btnBack").disabled = true;
-}
-
 // Event Listener (click) for the back button to return to the previous page in the browser history
 document.getElementById("btnBack").addEventListener("click", function () {
   window.history.back();
