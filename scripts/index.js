a3f91e4ac614ffc18e3f72777c52f179402d388b
VS-LIU <vctl95@gmail.com>
2023-05-23 14:31:16 -0700

    Add more elements to frontend

--- scripts/index.js
+++ scripts/index.js
@@ -9,8 +9,12 @@ function saveLanguageAndShowSignup() {
   const loginButton = document.getElementById("login-button");
   const signupButton = document.getElementById("signup-button");
   const backgroundImage = document.getElementById("backgroundCard");
+  const backgroundImageB = document.getElementById("backgroundCardB");
+  const backgroundImageC = document.getElementById("backgroundCardC");
   loginButton.style.display = "inline-block";
   signupButton.style.display = "inline-block";
   backgroundImage.style.display = "block";
+  backgroundImageB.style.display = "block";
+  backgroundImageC.style.display = "block";
 }
 
