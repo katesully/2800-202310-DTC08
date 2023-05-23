490cfdb49bf8a93600dc09da4c946a1bd62fe3cf
VS-LIU <vctl95@gmail.com>
2023-05-23 14:32:10 -0700

    Remove redundant code

--- scripts/index.js
+++ scripts/index.js
@@ -8,13 +8,7 @@ function saveLanguageAndShowSignup() {
   // Show login and signup buttons and info div
   const loginButton = document.getElementById("login-button");
   const signupButton = document.getElementById("signup-button");
-  const backgroundImage = document.getElementById("backgroundCard");
-  const backgroundImageB = document.getElementById("backgroundCardB");
-  const backgroundImageC = document.getElementById("backgroundCardC");
   loginButton.style.display = "inline-block";
   signupButton.style.display = "inline-block";
-  backgroundImage.style.display = "block";
-  backgroundImageB.style.display = "block";
-  backgroundImageC.style.display = "block";
 }
 
